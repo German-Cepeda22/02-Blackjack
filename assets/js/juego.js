5e7292d2e2329900a626bf298b9cda679e528a19
German-Cepeda22 <gacepedad67@soy.sena.edu.co>
2024-11-21 16:43:24 -0500

    Agregando avances del Juego

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -10,6 +10,15 @@ let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+//referencias HTML
+const btnPedir = document.querySelector('#btnPedir');
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+const puntosHTML = document.querySelectorAll('small');
 
 // Esta función crea una nueva baraja de cartas
 const crearDeck = () => {
@@ -43,9 +52,8 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop();
-    console.log(deck);
-    console.log(carta);
-    
+    // console.log(deck);
+    // console.log(carta);
     return carta;
     }
 
@@ -59,10 +67,34 @@ const valorCarta = ( carta ) => {
     const valor = carta.substring(0, carta.length - 1); // Elimina el último caracter de la carta
     return ( isNaN( valor)) ?
             ( valor === 'A') ? 11 : 10
-            : valor * 1;
+            : valor * 1; // Convierte el valor de la carta a un número
     
 
 }
 
-const valor = valorCarta (pedirCarta()); // Llamado a la función para tomar una carta
-console.log({valor}); // Llamado a la función para tomar el valor de una carta
+// const valor = valorCarta (pedirCarta()); // Llamado a la función para tomar una carta
+// console.log({valor}); // Llamado a la función para tomar el valor de una carta
+
+//Eventos
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+
+    puntosHTML[0].innerText = puntosJugador;
+
+    // <img class="carta" src="assets/cartas/2C.png"></img>
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append( imgCarta );
+
+    if ( puntosJugador > 21 ) {
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+    } else if ( puntosJugador === 21 ) {
+        console.warn('21, genial');
+        btnPedir.disabled = true;
+    }
+
+});
